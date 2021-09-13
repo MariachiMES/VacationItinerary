1f7b05b82125431dacdfc380b6570f4306259526
David J Ortiz <87109541+MariachiMES@users.noreply.github.com>
2021-09-13 11:23:50 -0500

    script amended to restore fetch functionality. no errors logging to console on page load

--- script.js
+++ script.js
@@ -51,16 +51,17 @@ var displayDays = function () {
   }
 };
 
-$(function () {
-  $("#datepicker").datepicker();
-});
+// $(function () {
+//   $("#datepicker").datepicker();
+// });
+var apiKey = "116296867a8e5f7080e808d86644669a";
 
 var fetchWeather = function (city) {
   fetch(
     "http://api.openweathermap.org/data/2.5/forecast?q=" +
       city +
       "&units=imperial&appid=" +
-      weatherApiKey
+      apiKey
   )
     .then((response) => response.json())
     .then((data) => displayWeather(data));
@@ -84,6 +85,7 @@ var displayWeather = function (data) {
 };
 
 var weatherSearch = function () {
+  console.log("pressed");
   if (document.querySelector("#city-name").value != " ") {
     fetchWeather(document.querySelector("#city-name").value);
   }
@@ -93,7 +95,6 @@ var alternateTripApi =
   "5ae2e3f221c38a28845f05b60ade91485de3f230f12f105b7c087b90";
 
 var tripApi = "5ae2e3f221c38a28845f05b627a67175ff5888a5fca032db41baf3b1";
-var apiKey = "5ae2e3f221c38a28845f05b61b840da041916e6dd029d0db7a8f4003";
 
 function fetchCityInfo(lon, lat) {
   fetch(
@@ -108,9 +109,9 @@ function fetchCityInfo(lon, lat) {
     .then((data) => displayPlaces(data));
 }
 var displayPlaces = function (data) {
-  console.log(data);
   for (var i = 0; i < 10; i++) {
     document.querySelector(`#place-${i}`).innerText = data[i].name;
   }
 };
+
 document.querySelector("#go-time").addEventListener("click", weatherSearch);
