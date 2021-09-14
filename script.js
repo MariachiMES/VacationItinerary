f7fcf770a7bdfbfe3f4e811da4ffdb892188ad81
Francisco Almaraz <frankcinq@yahoo.com>
2021-09-14 08:59:05 -0500

    updates

--- script.js
+++ script.js
@@ -1,49 +1,3 @@
-var draggables = document.querySelectorAll(".activity-card");
-var containers = document.querySelectorAll(".five-things-to-do");
-
-draggables.forEach((draggable) => {
-  draggable.addEventListener("dragstart", () => {
-    draggable.classList.add("dragging");
-  });
-
-  draggable.addEventListener("dragend", () => {
-    draggable.classList.remove("dragging");
-  });
-});
-
-containers.forEach((container) => {
-  container.addEventListener("dragover", (e) => {
-    e.preventDefault();
-    var afterElement = getDragAfterElement(container, e.clientY);
-    var draggable = document.querySelector(".dragging");
-    console.log(afterElement);
-    if (afterElement == null) {
-      container.appendChild(draggable);
-    } else {
-      container.insertBefore(draggable, afterElement);
-    }
-  });
-});
-
-function getDragAfterElement(container, y) {
-  var draggableElements = [
-    ...container.querySelectorAll(".draggable:not(.dragging)"),
-  ];
-
-  return draggableElements.reduce(
-    (closest, child) => {
-      var box = child.getBoundingClientRect();
-      var offset = y - box.top - box.height / 2;
-      if (offset < 0 && offset > closest.offset) {
-        return { offset: offset, element: child };
-      } else {
-        return closest;
-      }
-    },
-    { offset: Number.NEGATIVE_INFINITY }
-  ).element;
-}
-
 var displayDays = function () {
   for (var i = 0; i < 5; i++) {
     document.querySelector(`#day-${i}`).innerText =
@@ -114,3 +68,49 @@ var displayPlaces = function (data) {
   }
 };
 document.querySelector("#go-time").addEventListener("click", weatherSearch);
+
+var draggables = document.querySelectorAll(".activity-card");
+var containers = document.querySelectorAll(".five-things-to-do");
+
+draggables.forEach((draggable) => {
+  draggable.addEventListener("dragstart", () => {
+    draggable.classList.add("dragging");
+  });
+
+  draggable.addEventListener("dragend", () => {
+    draggable.classList.remove("dragging");
+  });
+});
+
+containers.forEach((container) => {
+  container.addEventListener("dragover", (e) => {
+    e.preventDefault();
+    var afterElement = getDragAfterElement(container, e.clientY);
+    var draggable = document.querySelector(".dragging");
+    console.log(afterElement);
+    if (afterElement == null) {
+      container.appendChild(draggable);
+    } else {
+      container.insertBefore(draggable, afterElement);
+    }
+  });
+});
+
+function getDragAfterElement(container, y) {
+  var draggableElements = [
+    ...container.querySelectorAll(".draggable:not(.dragging)"),
+  ];
+
+  return draggableElements.reduce(
+    (closest, child) => {
+      var box = child.getBoundingClientRect();
+      var offset = y - box.top - box.height / 2;
+      if (offset < 0 && offset > closest.offset) {
+        return { offset: offset, element: child };
+      } else {
+        return closest;
+      }
+    },
+    { offset: Number.NEGATIVE_INFINITY }
+  ).element;
+}
